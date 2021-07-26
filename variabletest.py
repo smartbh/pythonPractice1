aab8abba60ef6b8f4d344cc25270857ae21e36b2
smartbh <smartbh@naver.com>
2021-07-27 00:18:28 +0900

    python practice

---
+++ variabletest.py
@@ -0,0 +1,22 @@
+#variabletest.py
+
+x = 100
+
+print("x")
+print(x)
+
+x = 200
+print(x)
+
+y = 300
+print(y)
+
+y = 500
+print(y)
+
+z = x + y
+
+print(z)
+
+print("200 + 500 = ", z)
+print(x ,"+", y ,"=",z)
---
+++ variabletest2.py
@@ -0,0 +1,8 @@
+a= 500
+b= 600
+
+print( a,"+",b,"=",a+b)
+print( a,"-",b,"=",a-b)
+print( a,"*",b,"=",a*b)
+print( a,"/",b,"=",a/b)
+print( a,"//",b,"=",a//b)
